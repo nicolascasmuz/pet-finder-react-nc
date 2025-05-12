ca9ab97fe4d15ccf28aa2fb79d65a9519ebe5cd0
nicolascasmuz <nicolascasmuz@hotmail.com>
2025-05-12 17:26:54 -0300

    commit-014

--- src/API_BASE_URL.ts
+++ src/API_BASE_URL.ts
@@ -1,4 +1,5 @@
 // var API_BASE_URL = "https://pet-finder-react-nc-backend.onrender.com";
-var API_BASE_URL = "http://localhost:3000";
+var API_BASE_URL = "pet-finder-react-nc-backend-production.up.railway.app";
+// var API_BASE_URL = "http://localhost:3000";
 
 export { API_BASE_URL };
